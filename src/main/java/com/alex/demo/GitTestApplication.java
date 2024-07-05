3add61e8d12997db1ce4839b1629e18978d3eebc
王书高 <c-wangsg01@vanke.com>
2024-07-05 18:01:37 +0800

    整合nacos

--- src/main/java/com/alex/demo/GitTestApplication.java
+++ src/main/java/com/alex/demo/GitTestApplication.java
@@ -3,7 +3,9 @@ package com.alex.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.alex.demo.mapper")
 public class GitTestApplication {
